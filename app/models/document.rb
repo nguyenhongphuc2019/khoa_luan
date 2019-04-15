e47eab6b8488cf18bae3891891210bdedc92bce6
nguyenhongphuc2018 <nguyen.hong.phuc@framgia.com>
2019-04-15 19:04:11 +0700

    tracking view and dynamic suggest document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,9 @@
 class Document < ApplicationRecord
+  include Impressionist::IsImpressionable
   belongs_to :category
   mount_uploader :source_file, SourceFileUploader, dependent: :destroy
   mount_uploader :image, ImageUploader, dependent: :destroy
   has_many :feature_user_documents, dependent: :destroy
   belongs_to :language
+  is_impressionable counter_cache: :true, column_name: :total_view, unique: :request_hash
 end
